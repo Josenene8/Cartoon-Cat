571ff77ec2248fd955a7e930ecf77735ef5f4bf5
Daninnocent <58911390+Daninnocent@users.noreply.github.com>
2021-11-16 08:09:35 +0700

    Update TitleState.hx

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -95,14 +95,10 @@ class TitleState extends MusicBeatState
 		#elseif CHARTING
 		FlxG.switchState(new ChartingState());
 		#else
-		#if !cpp
 		new FlxTimer().start(1, function(tmr:FlxTimer)
 		{
 			startIntro();
 		});
-		#else
-		startIntro();
-		#end
 		#end
 	}
 
