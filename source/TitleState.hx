04546c9655047f98430dcf0bf054d1d6a8811ae3
Josenene8 <100051827+Josenene8@users.noreply.github.com>
2022-04-20 11:33:12 -0300

    Update TitleState.hx

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -21,7 +21,7 @@ import flixel.tweens.FlxEase;
 import flixel.tweens.FlxTween;
 import flixel.util.FlxColor;
 import flixel.util.FlxTimer;
-import io.newgrounds.NG;
+//  import io.newgrounds.NG;
 import lime.app.Application;
 import openfl.Assets;
 
@@ -250,11 +250,11 @@ class TitleState extends MusicBeatState
 		if (pressedEnter && !transitioning && skippedIntro)
 		{
 			#if !switch
-			NGio.unlockMedal(60960);
+	//		NGio.unlockMedal(60960);
 
 			// If it's Friday according to da clock
 			if (Date.now().getDay() == 5)
-				NGio.unlockMedal(61034);
+		//		NGio.unlockMedal(61034);
 			#end
 
 			FlxG.camera.flash(FlxColor.WHITE, 2);
